a9a7df3222abacbad20a43cf937445e58072d5d0
andrew <cole.zubac@gmail.com>
2024-05-30 23:47:17 -0400

    file setup and connection

---
+++ models/thought.js
@@ -0,0 +1,69 @@
+const { Schema, model, Types } = require('mongoose');
+// import moment module to format the timestamp 
+const moment = require('moment')
+
+//reaction schema
+const reactionSchema = new Schema (
+    {
+       reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+       },
+       reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+       },
+       username: {
+        type: String,
+        required: true,
+       },
+       createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+       },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    }
+)
+
+// thought schema
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const reaction = model('reaction', reactionSchema);
+const thought = model('thought', thoughtSchema);
+
+module.exports = reaction;
+module.exports = thought;
