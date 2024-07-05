b853c8e9bd9a8aaf709ea9027635e7ba7f8830a0
andrew <cole.zubac@gmail.com>
2024-07-04 20:49:21 -0400

    added index file to routes for export

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,4 @@
 const { Schema, model, Types } = require('mongoose');
-// import moment module to format the timestamp 
 const moment = require('moment')
 
 //reaction schema
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-// Imports
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
-// Middleware
 router.use("/api", apiRoutes);
 
 router.use((req, res) => res.send("Wrong route!"));
 
-// Exports
 module.exports = router;
